3ed6b1d9f1505b850c165cdcedc91c0839647939
daliabdmalek <49783051+daliabdmalek@users.noreply.github.com>
2019-05-08 12:41:20 +0200

    Updates

---
+++ precache-manifest.39251d1fee71fbbf2fd4b0ea2fafeb4b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a5987826724fd730daafb6c14d518b",
+    "url": "/CREDIT-CARD/index.html"
+  },
+  {
+    "revision": "e34272f1622a703295b0",
+    "url": "/CREDIT-CARD/static/css/main.f883256b.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/CREDIT-CARD/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "e34272f1622a703295b0",
+    "url": "/CREDIT-CARD/static/js/main.4dadcc81.chunk.js"
+  },
+  {
+    "revision": "8a1278978dad60a4de51",
+    "url": "/CREDIT-CARD/static/js/runtime~main.3a33429c.js"
+  },
+  {
+    "revision": "e18dda563e2317a7dcd35e394134c92f",
+    "url": "/CREDIT-CARD/static/media/37145cda8dd62aeeb2bdc1242549b76c.e18dda56.png"
+  },
+  {
+    "revision": "a4143a28803ba9bb42a536cbc8e78939",
+    "url": "/CREDIT-CARD/static/media/CreditCard.png1.a4143a28.png"
+  },
+  {
+    "revision": "a8c8030be44fdafe49518bd5325953a4",
+    "url": "/CREDIT-CARD/static/media/pus.a8c8030b.png"
+  }
+]);
